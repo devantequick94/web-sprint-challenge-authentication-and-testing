164d813f7ca0b6fad5b67223def045d286ef49c8
devantequick <devantequick94@gmail.com>
2022-05-26 20:33:09 -0400

    mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../users/users-model');
+const { validateBody, validateUsername } = require('../middleware/middleware');
+const { JWT_SECRET } = require('../secrets');
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post('/register', validateBody, (req, res, next) => {
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
     DO NOT EXCEED 2^8 ROUNDS OF HASHING!
-
     1- In order to register a new account the client must provide `username` and `password`:
       {
         "username": "Captain Marvel", // must not exist already in the `users` table
         "password": "foobar"          // needs to be hashed before it's saved
       }
-
     2- On SUCCESSFUL registration,
       the response body should have `id`, `username` and `password`:
       {
@@ -20,40 +22,59 @@ router.post('/register', (req, res) => {
         "username": "Captain Marvel",
         "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
       }
-
     3- On FAILED registration due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
-
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  User.add({ username, password: hash })
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(next);
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', validateUsername, (req, res, next) => {
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
-
     1- In order to log into an existing account the client must provide `username` and `password`:
       {
         "username": "Captain Marvel",
         "password": "foobar"
       }
-
     2- On SUCCESSFUL login,
       the response body should have `message` and `token`:
       {
         "message": "welcome, Captain Marvel",
         "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
       }
-
     3- On FAILED login due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
-
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+  if(bcrypt.compareSync(req.body.password, req.user[0].password)) {
+    const token = generateToken(req.user)
+    res.json({ message: `welcome, ${req.user[0].username}`, token })
+  } else if(!bcrypt.compareSync(req.body.password, req.user[0].password)) {
+    res.status(401).json({ message: "invalid credentials" })
+  } else {
+    next({ status: 401, message: 'invalid credentials' })
+  }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, JWT_SECRET , options)
+}
 
 module.exports = router;
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,34 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets'); 
+
+
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if(!token) {
+    res.status(401).json({ message: "token required" })
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if(err) {
+      res.status(401).json({ message: "token invalid"  })
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
+}
+
+module.exports = {
+  restricted
+}
+
+/*
     IMPLEMENT
-
     1- On valid token in the Authorization header, call next.
-
     2- On missing token in the Authorization header,
       the response body should include a string exactly as follows: "token required".
-
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
